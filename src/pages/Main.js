3fc49970b7c49bd4d87a2d74015c830da3a12ef6
Recepguc <recep.gucc@gmail.com>
2022-05-07 00:51:14 +0300

    main js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+
+const API_KEY = "2b2c46b37fbf259f5968db7bd2a5607c";
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
 
-export default Main
+  const getMovies = (API) => {
+    axios
+      .get(API)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
+  return <div>Main</div>;
+};
+
+export default Main;
