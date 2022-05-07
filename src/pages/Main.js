72b5dbd8b233979ed1326c8ba9941d8e6fcb9100
Recepguc <recep.gucc@gmail.com>
2022-05-07 21:00:36 +0300

    main js upt

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,18 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import MovieCard from "../component/MovieCard";
+import { AuthContext } from "../context/AuthContext";
+import { toastWarnNotify } from "../helpers/ToastNotify";
 
 const API_KEY = "2b2c46b37fbf259f5968db7bd2a5607c";
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+  const [searchTerm, setSearchTerm] = useState();
+
   useEffect(() => {
     getMovies(FEATURED_API);
   }, []);
@@ -13,11 +20,42 @@ const Main = () => {
   const getMovies = (API) => {
     axios
       .get(API)
-      .then((res) => console.log(res))
+      .then((res) => setMovies(res.data.results))
       .catch((err) => console.log(err));
   };
 
-  return <div>Main</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm && currentUser) {
+      getMovies(SEARCH_API + searchTerm);
+    } else if (!currentUser) {
+      toastWarnNotify("Please log in to search a movie");
+      // alert("Please log in to search a movie");
+    } else {
+      toastWarnNotify("Please enter a text");
+      // alert("Please enter a text");
+    }
+  };
+
+  return (
+    <div>
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Search a movie..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="d-flex justify-content-center flex-wrap">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} {...movie} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
